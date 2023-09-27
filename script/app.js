36740ecc2cd2d22073077ff091cdb04630a4e711
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2023-09-27 15:01:15 +0200

    added password toggle

--- script/app.js
+++ script/app.js
@@ -1,14 +1,28 @@
 const handlePasswordSwitcher = function () {
-    console.log("handlePasswordSwitcher executed");
-}
+  console.log('handlePasswordSwitcher executed');
+  const passwordInput = document.querySelector('.js-password-input');
+  const passwordToggle = document.querySelector('.js-password-toggle-checkbox');
+  const passwordOptions = ['password', 'text'];
+  passwordToggle.addEventListener('change', function () {
+    passwordInput.type = passwordOptions[+this.checked];
+
+    // passwordToggle.addEventListener('click', function () {
+    //   if (passwordInput.type == 'text') {
+    //     passwordInput.type = 'password';
+    //   } else if ((passwordInput.type = 'password')) {
+    //     passwordInput.type = 'text';
+    //   }
+    // });
+  });
+};
 
 const init = function () {
-    console.log('Script loaded!');
+  console.log('Script loaded!');
 
-    // init password switcher
-    handlePasswordSwitcher();
+  // init password switcher
+  handlePasswordSwitcher();
 
-    // Other init functions here
-}
+  // Other init functions here
+};
 
 init();
